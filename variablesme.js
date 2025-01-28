841c92283ae0f361ccc7f6a5c397cf242656aedb
gungunsaluja <gungunsaluja16@gmail.com>
2025-01-28 14:52:08 +0530

    basics js

--- variablesme.js
+++ variablesme.js
@@ -1,14 +1,31 @@
 
 // console.log(greetings);
-const ac = "hello";
-let ah = "world";
-var oh = "content";
-let ab = 12;
-console.table([ac,ah,oh,ab]);
-// by using this u can simply use all the variables at same time 
-// alert(3+2); we are using node not browser so that we can not use this ;
-let score = "a124";
-let score1 = Number(score);
-console.log(typeof score1); // type will be converted but it will be not a number 
+// const ac = "hello";
+// let ah = "world";
+// var oh = "content";
+// let ab = 12;
+// console.table([ac,ah,oh,ab]);
+// // by using this u can simply use all the variables at same time 
+// // alert(3+2); we are using node not browser so that we can not use this ;
+// let score = "a124";
+// let score1 = Number(score);
+// console.log(typeof score1); // type will be converted but it will be not a number 
 
-console.log(score1);
+// console.log(score1);
+// let b = 1;
+// let c = Boolean(b);
+// console.log(c);
+
+// 1 true 0 false; 
+// boolean = "hii"
+// console.log(Number);
+// console.log(boolean)
+// ;
+console.log(null>0);
+console.log(null==0);
+console.log(null!=0);
+console.log(undefined==0);
+console.log(undefined!=0);
+console.log("2"===2);
+// primitive
+
