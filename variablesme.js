b22081ad881faa4ee8bb07f7702724c9823138ff
gungunsaluja <gungunsaluja16@gmail.com>
2025-01-28 12:54:44 +0530

    basic js

--- variablesme.js
+++ variablesme.js
@@ -1,2 +1,14 @@
-var greetings = "hello Gungun";
-console.log(greetings);
+
+// console.log(greetings);
+const ac = "hello";
+let ah = "world";
+var oh = "content";
+let ab = 12;
+console.table([ac,ah,oh,ab]);
+// by using this u can simply use all the variables at same time 
+// alert(3+2); we are using node not browser so that we can not use this ;
+let score = "a124";
+let score1 = Number(score);
+console.log(typeof score1); // type will be converted but it will be not a number 
+
+console.log(score1);
